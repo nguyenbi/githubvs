f8f56024a1e6050fb01fcf5bd73e239da4649da8
nguyenbi <abc@gmail.com>
2023-02-12 11:36:29 +0700

    Day la file thay doi

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page 123.";
 
             return View();
         }
